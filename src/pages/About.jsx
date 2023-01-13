5fa233118719fb818548c6675b434caffaf89d1b
alefagundes <alefagundes2000.af@gmail.com>
2023-01-13 00:24:01 -0300

    change Link in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,5 @@
 import style from './About.module.scss'
 import { SiPokemon } from 'react-icons/si'
-import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
@@ -10,9 +9,9 @@ const About = () => {
       </h1>
       <p>
         Projeto desenvolvido com <span className={style.react}>React.JS</span>{' '}
-        utilizando a <Link to="https://pokeapi.co/">PokéAPI</Link> e hospedado
-        na Vercel com o objetivo de demostrar meus conhecimentos com essa
-        excelente biblioteca!
+        utilizando a <a href="https://pokeapi.co/">PokéAPI</a> e hospedado na
+        Vercel com o objetivo de demostrar meus conhecimentos com essa excelente
+        biblioteca!
       </p>
       <img src="/poke.png" alt="poke" />
     </div>
